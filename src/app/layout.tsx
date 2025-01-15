73dc870e146d314a23a9f9b939395adadc9a5386
Shel Soloa <shel@shelsoloa.com>
2025-01-15 15:56:20 -0500

    Base layout & design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+// import bg from "@/bg1.JPG";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +26,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        // className={`${geistSans.variable} ${geistMono.variable} antialiased bg-stone-200`}
+        style={{
+          backgroundImage: `url(/bg3.JPG)`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
       >
         {children}
       </body>
